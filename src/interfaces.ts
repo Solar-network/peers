32b10cd1e52129e2797f81ba3b8004c47c91e0cc
Nayiem <info@hodlercompany.com>
2022-02-19 21:12:32 +0100

    chore: migrate structure

---
+++ src/interfaces.ts
@@ -0,0 +1,17 @@
+export interface IPeer {
+	ip: string;
+	port: number;
+	ports?: Record<string, number>;
+	version?: string;
+	height?: number;
+	latency?: number;
+}
+
+export interface IPeerResponse {
+	ip: string;
+	port: number;
+	ports: Record<string, number>;
+	version: string;
+	height: number;
+	latency: number;
+}
